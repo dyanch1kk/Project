8c13df9d01e0d194891039e7b7f4f627e3ae22f6
Diana <dyanchikk@gmail.com>
2024-05-26 15:05:34 +0300

    3. добавила кнопку выбора режима

--- src/main/java/org/example/project/GameApplication.java
+++ src/main/java/org/example/project/GameApplication.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.control.ComboBox;
 
 public class GameApplication extends Application {
     private Button buttonstart; //кнопка старта
@@ -15,6 +16,8 @@ public class GameApplication extends Application {
     // счет будет считаться так:
     // если нажали на шарик, +1 к счету
     // если промахнулись, -1 к счету
+    private BorderPane gamePane;
+    private ComboBox<String> modeComboBox;
     @Override
     public void start(Stage Stage1) {
         Stage1.setTitle("Aim lab");
@@ -22,10 +25,20 @@ public class GameApplication extends Application {
         time = new Label("Время: 0");
         score = new Label("Счет: 0");
         FlowPane top = new FlowPane();
+        gamePane = new BorderPane();
+        modeComboBox = new ComboBox<>();
+        modeComboBox.getItems().addAll("Простой", "Средний", "Сложный");
+        modeComboBox.setValue("Простой"); // по дефолту какой режим
         top.setHgap(200); //горизонтальное расстояние между элементами
         top.getChildren().addAll(buttonstart, time, score);
+        FlowPane bottomPanel = new FlowPane();
+        bottomPanel.setHgap(10);
+        bottomPanel.getChildren().addAll(new Label("Мод:"), modeComboBox);
         BorderPane pane = new BorderPane(); // панель
         pane.setTop(top); // отступ
+        pane.setBottom(bottomPanel);
+        pane.setCenter(gamePane);
+
         Scene scene = new Scene(pane, 800, 600);
         Stage1.setScene(scene);
         Stage1.show();
